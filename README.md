3feca7fca199a1c0ff20dbefa7db504603e15f27
Sneha16-coder <snehamaisalge506@gmail.com>
2025-06-02 15:05:09 +0530

    vending project

---
+++ README.md
@@ -0,0 +1,1 @@
+# smart-vending-machine
---
+++ frontend/firebase-config.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFumMkI_Ov2eIa_izDy3CZwfbYQPVdZNw",
+  authDomain: "smart-vending-machine-66e22.firebaseapp.com",
+  projectId: "smart-vending-machine-66e22",
+  storageBucket: "smart-vending-machine-66e22.firebasestorage.app",
+  messagingSenderId: "483676574873",
+  appId: "1:483676574873:web:3e22334043f1e5f84f9408",
+  measurementId: "G-BKHBCWJSZV"
+};
+
